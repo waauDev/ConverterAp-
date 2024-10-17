941961b120f2e426e9b6da5d46aa685ae136a133
William Acuña <unknow.citizen@protonmail.com>
2024-10-16 22:45:46 -0500

    Se agregan verificaciones de try catch sobre formato del numero

--- src/Principal.java
+++ src/Principal.java
@@ -10,7 +10,9 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.nio.DoubleBuffer;
 import java.sql.SQLOutput;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Principal {
@@ -41,25 +43,59 @@ public class Principal {
 
         while (opcion != 7){
             System.out.println(menu);
-            opcion = teclado.nextInt();
+            boolean bandera =false;
+
+            while (!bandera){
+                try {
+                    opcion = teclado.nextInt();
+                    bandera = true;
+                }catch (InputMismatchException e){
+                    System.out.println("Digite por favor un número de acuerdo al menu");
+                    teclado.next();
+                }
+            }
+            teclado.nextLine();
 
             switch (opcion){
                 case 1:
                     System.out.println("Entrando a opcion 1");
                     System.out.println("Ingrese el valor que deseas convertir: ");
-                    Double monedaCambio = (double) teclado.nextInt();
-                    String parametro1="USD";
-                    String parametro2="ARS";
 
-                    String direccion = baseUrl + parametro1 +"/"+parametro2+"/"+ monedaCambio;
+                    String monedaCambio = teclado.nextLine();
+                    System.out.println(monedaCambio);
+
+                    ///Verifico si el dato ingresado es un valor numerico
+                    boolean banderaIn =false;
+                    Double checkNum ;
+                    while (!banderaIn) {
+                        try {
+                            checkNum = Double.valueOf(monedaCambio);
+                            //System.out.println(checkNum);
+                            banderaIn = true;
+
+                        } catch (NumberFormatException e) {
+                            System.out.println("Por favor ingrese valores numericos y/o decimales:\n"+ e.getMessage());
+                            monedaCambio = teclado.nextLine();
+                        }
+                    }
+                    //System.out.println(monedaCambio);
+
+                    String parametro1 = "USD";
+                    String parametro2 = "ARS";
+
+                    String direccion = baseUrl + parametro1 + "/" + parametro2 + "/" + monedaCambio;
                     //System.out.println(direccion);
+
                     String valorMonedaStr = String.valueOf(monedaCambio);
                     ApiCall apiCall = new ApiCall();
 
-                    apiCall.fetchData(direccion,valorMonedaStr);
+                    apiCall.fetchData(direccion, valorMonedaStr);
+
+                    }
+
 
             }
-        }
+
 
 
 
